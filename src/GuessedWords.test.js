6c9cf6811508ab4f602efb62c3921a97d94f0891
keatongallagher <keatongallagher@its-MacBook-Pro.local>
2020-05-22 11:29:04 -0600

    Started writing tests for if there are already guessed words

--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -41,4 +41,33 @@ describe('If no words are guessed', () => {
   });
 });
 
-describe('If there are words guesssed.', () => {});
+describe('If there are words guesssed.', () => {
+  let wrapper;
+  const guessedWords = [
+    {guessedWord:'Fire', letterMatchCount:3},
+    {guessedWord:'Water', letterMatchCount:1},
+    {guessedWord:'Air', letterMatchCount:1},
+    {guessedWord:'Earth', letterMatchCount5},
+  ]
+  beforeEach(() => {
+    wrapper = setup({guessedWords})
+  })
+
+  test('Renders without error', () => {
+    const component = findByTestAttr(wrapper, 'guessed-words-component')
+    expect(component.length).toBe(1)
+  })
+  test('Renders guessed words section', () => {
+    const guessedWordsNode = findByTestAttr(wrapper, 'guessed-words')
+    expect(guessedWordsNode.length).toBe(1)
+  })
+
+  test('Displays the correct number of guessed words', () => {
+    const guessedWordsNodes = findByTestAttr(wrapper, 'guessed-word')
+    expect(guessedWordsNodes.length).toBe(guessedWords.length)
+
+
+  })
+  
+  
+});
